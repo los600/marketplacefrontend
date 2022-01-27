54f38867a09663a9c4a4356f53593a7c9473ea2c
Rubén <rubencaporte@gmail.com>
2022-01-27 12:58:18 +0100

    form create connect to db

--- src/components/create.js
+++ src/components/create.js
@@ -1,34 +1,61 @@
-import React from 'react';
-/* import { useStyles } from 'react'; */
-import { makeStyles } from '@material-ui/core/styles';
-import  TextField  from '@material-ui/core/TextField';
-import { Container, Paper } from '@material-ui/core';
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import { Container, Paper, Button } from "@material-ui/core";
 
-
-const useStyles = makeStyles ((theme) =>({
-    root:{
-        '& > *' : {
-            margin: theme.spacing(1),
-            
-        },
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
     },
+  },
 }));
 
 export default function Create() {
-    const paperStyle={padding: '50px 20px', width:600, margin: "20px auto"}
-    const classes = useStyles();
+  const paperStyle = { padding: "50px 20px", width: 600, margin: "20px auto" };
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const classes = useStyles();
+  const handleClick = (e) => {
+    e.preventDefault();
+    const Product = { name, price };
+    console.log(Product);
 
-    return (
-        <Container>
-            <Paper elevation={3} style={paperStyle}>
-        <form className={classes.root} noValidate autoComplete ="off">  
-        
-        <TextField id= "outlined-basic" label="Product Name" variant= "outlined" />
-        <TextField id= "outlined-basic" label="Product Price" variant= "outlined" /> 
-        </form>    
-        </Paper>
-        </Container>   
-    );
+    fetch("http://localhost:8080/product/add", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(Product),
+    }).then(() => {
+      console.log("New Product added");
+    });
+  };
 
-
+  return (
+    <Container>
+      <Paper elevation={3} style={paperStyle}>
+        <h1 style={{ color: "blue" }}>Create Product</h1>
+        <form className={classes.root} noValidate autoComplete="off">
+          <TextField
+            id="outlined-basic"
+            label="Product Name"
+            variant="outlined"
+            fullWidth
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <TextField
+            id="outlined-basic"
+            label="Product Price"
+            variant="outlined"
+            fullWidth
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+          />
+          <Button variant="contained" onClick={handleClick}>
+            Submit
+          </Button>
+        </form>
+      </Paper>
+    </Container>
+  );
 }
