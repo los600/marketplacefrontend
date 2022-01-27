90cb04befbadaca5ff474e165afa1af8ab4114ce
EstherCastell <esthercastell2014@gmail.com>
2022-01-27 11:55:56 +0100

    error

--- src/components/create.js
+++ src/components/create.js
@@ -2,6 +2,7 @@ import React from 'react';
 /* import { useStyles } from 'react'; */
 import { makeStyles } from '@material-ui/core/styles';
 import  TextField  from '@material-ui/core/TextField';
+import { Container, Paper } from '@material-ui/core';
 
 
 const useStyles = makeStyles ((theme) =>({
@@ -14,14 +15,19 @@ const useStyles = makeStyles ((theme) =>({
 }));
 
 export default function Create() {
+    const paperStyle={padding: '50px 20px', width:600, margin: "20px auto"}
     const classes = useStyles();
 
     return (
+        <Container>
+            <Paper elevation={3} style={paperStyle}>
         <form className={classes.root} noValidate autoComplete ="off">  
         
         <TextField id= "outlined-basic" label="Product Name" variant= "outlined" />
         <TextField id= "outlined-basic" label="Product Price" variant= "outlined" /> 
-        </form>       
+        </form>    
+        </Paper>
+        </Container>   
     );
 
 
